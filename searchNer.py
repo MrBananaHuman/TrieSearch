5cd769cb67845b2951e284b58f9cf81b9a210f8f
MrBananaHuman <34882690+MrBananaHuman@users.noreply.github.com>
2019-10-22 14:03:06 +0900

    Add files via upload

---
+++ searchNer.py
@@ -0,0 +1,53 @@
+data = open('ne_dic.txt', 'r', encoding='utf-8')
+lines = data.readlines()
+
+class Node(object):
+    def __init__(self, text, tag=None, data=None):
+        self.text = text
+        self.tag = tag
+        self.data = data
+        self.children = {}
+
+class Trie(object):
+    def __init__(self):
+        self.head = Node(None)
+
+    def insert(self, text, tag):
+        current_node = self.head
+        for char in text:
+            if char not in current_node.children:
+                current_node.children[char] = Node(char)
+            current_node = current_node.children[char]
+        current_node.tag = tag
+        current_node.data = text
+
+    def search(self, text):
+        current_node = self.head
+        last_tag = None
+        tagged_text = None
+        for char in text:
+            if char in current_node.children:
+                current_node = current_node.children[char]
+            else:
+                break
+            if current_node.data != None and current_node.tag != None :
+                last_tag = current_node.tag
+                tagged_text = current_node.data
+        return last_tag, tagged_text
+
+
+t = Trie()
+
+tags = ['PS', 'OG', 'LC', 'PL', 'EV', 'PR', 'QT', 'DT', 'OC', 'TI']
+for line in lines:
+    line = line.replace('\n', '')
+    text = line.split(' ')[0]
+    tag = line.split(' ')[1]
+    if tag in tags:
+        t.insert(text, tag)
+
+while(True):
+    print('input')
+    a = input()
+    print('\n', t.search(a))
+
